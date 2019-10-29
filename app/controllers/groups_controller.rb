b499a0550285cfd4675a1211c0450fb5cb6e80c4
kjdy <56123914+kjdy@users.noreply.github.com>
2019-10-30 08:05:19 +0900

    update changes

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,10 +1,25 @@
 class GroupsController < ApplicationController
-  def new
+  def index
 
+  end
+  
+  def new
+    @group = Group.new
+    @group.users << current_user
   end
 
   def create
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to root_path, notice: 'グループを作成しました'
+    else
+      render :new
+    end
+  end
 
+  private
+  def group_params
+    params.require(:group).permit(:name, user_ids: [] )
   end
 
   def edit
@@ -14,6 +29,6 @@ class GroupsController < ApplicationController
   def update
 
   end
-  
+
 
 end
